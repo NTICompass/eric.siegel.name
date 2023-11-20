ebe4395b16b7a694e395faac95bb3adaec9a5e4e
Eric Siegel <esiegel@goboomtown.com>
2023-11-20 14:59:36 -0500

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,11 @@
+import {createApp} from 'vue';
+import App from './App.vue';
+import router from './router';
+
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
+// import {library} from '@fortawesome/fontawesome-svg-core';
+
+import './assets/main.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+createApp(App).use(router).component('FontAwesomeIcon', FontAwesomeIcon).mount('#app');
