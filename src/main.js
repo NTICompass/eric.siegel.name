087896594d174f7bd0172009e40202b1b7112114
Eric Siegel <esiegel@goboomtown.com>
2023-11-20 17:32:20 -0500

    add contact links

--- src/main.js
+++ src/main.js
@@ -3,7 +3,16 @@ import App from './App.vue';
 import router from './router';
 
 import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome';
-// import {library} from '@fortawesome/fontawesome-svg-core';
+import {library} from '@fortawesome/fontawesome-svg-core';
+
+import {faEnvelope, faComputer} from '@fortawesome/free-solid-svg-icons';
+import {faYoutube, faGithub, faLinkedin, faStackOverflow} from '@fortawesome/free-brands-svg-icons';
+
+/*
+ * <FontAwesomeIcon :icon="['fab', 'youtube']" />
+ * <FontAwesomeIcon :icon="['fas', 'key']" />
+ */
+library.add(faEnvelope, faComputer, faYoutube, faGithub, faLinkedin, faStackOverflow);
 
 import 'bootstrap/dist/css/bootstrap.css';
 
--- src/views/Contact.vue
+++ src/views/Contact.vue
@@ -1,10 +1,60 @@
 <script setup>
-
+const links = [
+    {
+        name: 'gen/Eric Computers',
+        link: 'https://generic.computers.pictures',
+        icon: ['fas', 'computer'],
+    },
+    {
+        name: 'GitHub',
+        link: 'https://github.com/nticompass',
+        icon: ['fab', 'github'],
+    },
+    {
+        name: 'LinkedIn',
+        link: 'https://linkedin.com/in/nticompass',
+        icon: ['fab', 'linkedin'],
+    },
+    {
+        name: 'Stack Overflow',
+        link: 'https://stackoverflow.com/users/206403/gen-eric',
+        icon: ['fab', 'stack-overflow'],
+    },
+    {
+        name: 'YouTube',
+        link: 'https://youtube.com/@nticompass',
+        icon: ['fab', 'youtube'],
+    },
+];
 </script>
 
 <template>
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">
+            <dl>
+                <dt>
+                    <FontAwesomeIcon :icon="['fas', 'envelope']" />
+                    Email
+                </dt>
+                <dd>
+                    <a href="mailto:eric@siegel.name">
+                        eric@siegel.name
+                    </a>
+                </dd>
+            </dl>
+        </li>
+        <li class="list-group-item">
+            <dl v-for="link in links" :key="link.name">
+                <dt>
+                    <FontAwesomeIcon :icon="link.icon" />
+                    {{ link.name }}
+                </dt>
+                <dd>
+                    <a :href="link.link" target="_blank">
+                        {{ link.link }}
+                    </a>
+                </dd>
+            </dl>
+        </li>
+    </ul>
 </template>
-
-<style scoped>
-
-</style>
