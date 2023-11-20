f75952dc88126f6e6dc58cc193fc2b937e43410b
Eric Siegel <esiegel@goboomtown.com>
2023-11-20 18:38:39 -0500

    add home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,6 +4,26 @@
 
 <template>
     <h1>Eric Siegel</h1>
+    <p class="lead">
+        Web Developer, Linux User, Vintage Computer Enthusiast
+    </p>
+    <p>
+        Hello, I'm Eric.  I'm a full-stack web developer with proficiency in PHP and JavaScript.
+        I am also skilled in the Linux command line and working with and configuring web servers using Apache or nginx.
+    </p>
+    <p>
+        I have worked with various JavaScript libraries such as Ext JS and Vue.js.
+        This website is actually built in Vue.js and deployed on to a server that I manage.
+    </p>
+    <p>
+        For personal projects, I use Python to create simple programs to help me complete tasks.
+        Tasks such as <a href="https://gist.github.com/NTICompass/f7692aa8b7ba536666c4dab878efbe97" target="_blank">solving a math puzzle</a>
+        or <a href="https://github.com/NTICompass/mms-viewer" target="_blank">manually downloading MMS messages on my old phone</a>.
+    </p>
+    <p>
+        One of my hobbies is collecting and working with vintage computers.  I have a large
+        <a href="https://generic.computers.pictures/computers" target="_blank">collection of systems</a> that I have restored or am working on restoring.
+    </p>
 </template>
 
 <style scoped>
